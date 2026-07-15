5d4856da18ca9bf381f5408dbc45d4e11724359c
5avire <savonghimire@gmail.com>
2026-07-15 22:38:43 +0545

    Tiny binds fix

--- config/hypr/modules/defaults.lua
+++ config/hypr/modules/defaults.lua
@@ -6,12 +6,12 @@ appPicker        = "rofi -show drun -config application.rasi"
 
 controlPanel     = ""
 
-volumeUp         = ""
-volumeDown       = ""
-volumeMuteToggle = ""
+volumeUp         = "wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+volumeDown       = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+volumeMuteToggle = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
 
-brightnessUp     = ""
-brightnessDown   = ""
+brightnessUp     = "brightnessctl -e4 -n2 set 5%+"
+brightnessDown   = "brightnessctl -e4 -n2 set 5%-"
 
 emojiPicker      = "rofimoji"
 clipboard        = "~/dots/scripts/clipboard.sh"
